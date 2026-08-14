5483e4b736ca3d2035d0acb65e216a22fb7b5e2c
afbufd <aflores4824@gmail.com>
2026-08-13 19:13:19 -0700

    Save data to local storage

--- 02-Movie-Seat-Booking/script.js
+++ 02-Movie-Seat-Booking/script.js
@@ -7,10 +7,23 @@ const movieSelect = document.getElementById('movie');
 // Turn String -> Integer
 let ticketPrice = parseInt(movieSelect.value);// Avengers returns 10 as a string
 
+// Save the selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //Update the seat count and total cost
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+ 
+    // Copy selected seats into arr
+    // Map through array
+    // return a new array of indexes\
+    const seatsIndex = [...selectedSeats].map( (seat) => [...seats].indexOf(seat));// indexOf gets the 0-index of the curr seat);
 
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
     const selectedSeatLength = selectedSeats.length;
     count.innerText = selectedSeatLength;
     total.innerText = selectedSeatLength * ticketPrice;
@@ -19,6 +32,7 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
