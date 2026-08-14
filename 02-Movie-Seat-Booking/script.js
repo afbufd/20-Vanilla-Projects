d2453f9bbfe58367cb80d1a63090e2a1247a8f6e
afbufd <aflores4824@gmail.com>
2026-08-13 18:53:39 -0700

    Movie & seat JS functionality

---
+++ 02-Movie-Seat-Booking/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied'); // All Acts like an array
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+// Turn String -> Integer
+let ticketPrice = parseInt(movieSelect.value);// Avengers returns 10 as a string
+
+//Update the seat count and total cost
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatLength = selectedSeats.length;
+    count.innerText = selectedSeatLength;
+    total.innerText = selectedSeatLength * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') 
+        && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});
+
+
+
+
+
+
+
+
