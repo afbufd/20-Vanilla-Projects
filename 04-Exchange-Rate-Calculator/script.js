8abb4b56d06aa7baf45ffb8c36d93b472c13a3f7
afbufd <aflores4824@gmail.com>
2026-08-18 17:16:47 -0700

    Add async function

--- 04-Exchange-Rate-Calculator/script.js
+++ 04-Exchange-Rate-Calculator/script.js
@@ -4,18 +4,16 @@ const currencyE2_two = document.getElementById('currency-two');
 const amountE2_two = document.getElementById('amount-two');
 const rateE1 = document.getElementById('rate');
 
-function calculate() {
+// Fetch exchange rates and update the DOM
+async function calculate() {
     const currency_one = currencyE1_one.value;
     const currency_two = currencyE2_two.value;
-    fetch("https://open.exchangerate-api.com/v6/latest")
-        .then(res => res.json())
-        .then(data => {
-            const rate = data.rates[currency_two] / data.rates[currency_one];
-            rateE1.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-            amountE2_two.value = (amountE1_one.value * (rate)).toFixed(2);
-        });
+    const response = await fetch(`https://open.exchangerate-api.com/v6/latest/${currency_one}`);
+    const data = await response.json();
+    const rate = (data.rates[currency_two] / data.rates[currency_one]);
+    rateE1.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+    amountE2_two.value = (amountE1_one.value * (rate)).toFixed(2);
 }
-
 
 // Event listeners
 currencyE1_one.addEventListener('change', calculate);
@@ -28,6 +26,6 @@ swap.addEventListener('click', () => {
     currencyE1_one.value = currencyE2_two.value;
     currencyE2_two.value = temp;
     calculate();
-})
+});
 
 calculate();
